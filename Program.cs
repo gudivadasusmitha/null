2d8445bdb1abf0827a5175b63df95ed9065df966
gudivadasusmitha <Gudivada.Susmitha@ibm.com>
2022-03-03 16:35:26 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @null
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = null;
+            string myname = name ?? "susmitha";
+            Console.WriteLine(myname);
+            Console.ReadLine();
+        }
+    }
+}
